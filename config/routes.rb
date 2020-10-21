3624083d08b29060fd01705d37d82d1ba3c8d0e7
Edwin Mak <edwinthinks@gmail.com>
2020-10-20 21:06:20 -0500

    Add bootstrap styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#landing'
-
-  get 'landing', to: 'pages#landing', format: true
 end
